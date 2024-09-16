37538b1da9295795d1986fe79b7c2700a8f8ee87
Noak838 <northon.carvalho@gmail.com>
2024-09-16 13:52:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
+import os
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 # Function to generate folder name based on user input
 def create_folder_name(answers):
-    # Replace empty answers with 'XXX' and format the folder name
-    answers = [answer if answer else "XXX" for answer in answers]
+    answers = [answer if answer != "" else "XXX" for answer in answers]
     folder_name = "_".join(answers).upper().replace(' ','')
     return folder_name
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Collect form data
         answers = [
             request.form.get('Modelo'),
             request.form.get('Litragem'),
             request.form.get('Potencia'),
             request.form.get('Ano'),
-            request.form.get('Transmissao'),
+            request.form.get('Transmissão'),
             request.form.get('Placa'),
             request.form.get('Cliente')
         ]
-        # Generate the folder name
         folder_name = create_folder_name(answers)
         return jsonify({'folder_name': folder_name})
 
-    # Render the HTML template for GET requests
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    port = int(os.environ.get('PORT', 8080))  # Use the PORT provided by Heroku or default to 8080
+    app.run(host='0.0.0.0', port=port, debug=True)
